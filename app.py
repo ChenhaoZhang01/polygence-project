212c8eb06e07a848edae8fd88d59cf232705f5d9
ChenhaoZhang01 <chenhaozhang01@gmail.com>
2024-08-04 09:45:11 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+# %% Modules
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+# %% Application
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/echo')
+def echo():
+    message = request.args.get('message')
+    if message:
+        return f'MESSAGE: {message}'
+    else:
+        return 'No message provided!'
+
