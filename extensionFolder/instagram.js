f56f2fb979fee41dd82593355cbc0bb3d43b098b
ChenhaoZhang01 <chenhaozhang01@gmail.com>
2024-08-15 20:57:24 -0700

    Add files via upload

---
+++ extensionFolder/instagram.js
@@ -0,0 +1,23 @@
+function trackMessage() {
+    const tweetTextarea = document.querySelector(`span.x3jgonx[data-lexical-text="true"]`);
+    console.log('Tweet Textarea:', tweetTextarea);
+    if (tweetTextarea) {
+        const tweetInput = tweetTextarea.innerText;
+        chrome.storage.local.set({ tweetInput: tweetInput }, function() {
+            if (chrome.runtime.lastError) {
+                console.error('Error:', chrome.runtime.lastError);
+            } else {
+                console.log('Tweet saved:', tweetInput);
+            }
+        });
+    } else {
+        console.log('Tweet text not found.');
+    }
+}
+
+
+//keydown code
+window.addEventListener('keypress', function(event) {
+    console.log("Key pressed"); 
+    trackMessage();   
+});
